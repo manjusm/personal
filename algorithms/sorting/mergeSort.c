89eda85f5e1619b6a850e668e082303e66f1daee
Manjunatha S M <sm.anjunatha@gmail.com>
2017-08-11 20:54:53 +0530

    Updated merge sort

--- algorithms/sorting/mergeSort.c
+++ algorithms/sorting/mergeSort.c
@@ -2,10 +2,55 @@
 
 #include <stdio.h>
 
+void merge(int input[], int low, int mid, int high)
+{
+    int i, j, k;
+    i = low;
+    j = mid + 1;
+    k = 0;
+    int temp[10] = {0};
+
+    while(i <= mid && j <= high) {
+        
+        if (input[i] <= input[j]){
+            temp[k++] = input[i++];
+        } else {
+            temp[k++] = input[j++];
+        }
+    }
+    while(i <= mid){
+        temp[k++] = input[i++];
+    }
+
+    while(j <= high){
+        temp[k++] = input[j++];
+    }
+
+    for (i = low, j = 0; i <= high; i++, j++){
+        input[i] = temp[j];
+    }
+
+    return;
+}
+
+int mergeSort(int input[], int low, int high)
+{
+    int mid;
+    if(low < high) {
+        mid = (low + high)/2;
+        /* Divide and conquor */
+        mergeSort(input, low, mid);
+        mergeSort(input, mid+1, high);
+        /* Combine */
+        merge(input, low, mid, high);
+    }
+
+    return 0;
+}
+
 int main()
 {
     int input[] = {10, 7, 4, 12, 8, 2, 13};
-    int output[7] = {0};
     int i, j, size;
 
     size = sizeof(input)/sizeof(input[0]);
@@ -16,12 +61,13 @@ int main()
     }
     printf("\n");
 
+    mergeSort(input, 0, size-1);
+
     printf("After sorting : ");
     for (int i = 0; i < size; i++){
-        printf("%d\t", output[i]);
+        printf("%d\t", input[i]);
     }
     printf("\n");
 
     return 0;
-
 }
