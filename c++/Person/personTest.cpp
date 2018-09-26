8dba08bb024e56e462d8eaebceb2d016c7e09418
Manjunatha S M <sm.manjunatha@gmail.com>
2018-09-26 13:39:49 +0530

    Updated to be completed.

--- c++/Person/personTest.cpp
+++ c++/Person/personTest.cpp
@@ -4,8 +4,17 @@
 using namespace std;
 
 int main(){
-	Person *pOne = new Person("Manjunatha", "SM", 30);
+	Person *pOne = new Person();
 	cout<<"Age : "<<pOne->getAge()<<endl;
+
+    Person *ptwo = new Person("Manju", "SM");
+    cout<<"Age : "<<ptwo->getAge()<<endl;
+
+    Person *pthree = new Person("Manju", "SM", 30);
+    cout<<"Age : "<<pthree->getAge()<<endl;
+
 	//delete calls destructor
 	delete pOne;
+    delete ptwo;
+    delete pthree;
 }
